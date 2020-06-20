e64722dc41d9136b3c262971670d68983542a44a
lyj157175 <lyj157175@163.com>
2020-06-21 00:01:29 +0800

    commit

--- project1 Bert-text-classification/train.py
+++ project1 Bert-text-classification/train.py
@@ -5,17 +5,26 @@ import utils
 import torch.nn.functional as F
 from sklearn import metrics
 import time
-from pytorch_pretrained.optimization import  BertAdam
+from pytorch_pretrained.optimization import BertAdam
+
 
 
 def train(config, model, train_iter, dev_iter, test_iter):
-
+    """
+    模型训练方法
+    :param config:
+    :param model:
+    :param train_iter:
+    :param dev_iter:
+    :param test_iter:
+    :return:
+    """
     start_time = time.time()
     #启动 BatchNormalization 和 dropout
     model.train()
     #拿到所有mode种的参数
     param_optimizer = list(model.named_parameters())
-    #不需要衰减的参数
+    #定义不需要衰减的参数，bert是基于transformer的encoder，add&norm层的参数不需要衰减
     no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
 
     optimizer_grouped_parameters = [
@@ -26,46 +35,47 @@ def train(config, model, train_iter, dev_iter, test_iter):
     ]
 
     optimizer = BertAdam(params = optimizer_grouped_parameters,
-                        lr = config.learning_rate,
-                        warmup = 0.5,
-                        t_total = len(train_iter) * config.num_epochs)
+                         lr=config.learning_rate,
+                         warmup=0.05,
+                         t_total=len(train_iter) * config.num_epochs)
 
     total_batch = 0 #记录进行多少batch
     dev_best_loss = float('inf') #记录校验集合最好的loss
-    last_improve = 0 #记录上次校验集loss下降的batch数
+    last_imporve = 0 #记录上次校验集loss下降的batch数
     flag = False #记录是否很久没有效果提升，停止训练
     model.train()
 
     for epoch in range(config.num_epochs):
-        print('epoch: {}/{}'.format((epoch+1), config.num_epochs))
-        # it = iter(train_iter)
-        for i,(trains, labels) in enumerate(train_iter): #labels.shape [128]
-            outputs = model(trains)  #shape [128, 10]
-            model.zero_grad
+        print('Epoch [{}/{}'.format(epoch+1, config.num_epochs))
+        for i, (trains, labels) in enumerate(train_iter):
+            outputs = model(trains)
+            model.zero_grad()
             loss = F.cross_entropy(outputs, labels)
             loss.backward()
             optimizer.step()
 
-            if total_batch % 100 == 0:
-                label = labels.data.cpu()
-                predict = torch.max(outputs.data, 1)[1].cup()  #max()[1]最大的索引
-                train_acc = metrics.accuracy_scorce(label, predict)
-                
+            #每多少次输出在训练集和校验集上的效果
+            if total_batch % 100 == 0: 
+                true = labels.data.cpu()
+                predit = torch.max(outputs.data, 1)[1].cpu()
+                #metrics.accuracy_score是sklearn上，在cpu上校验
+                train_acc = metrics.accuracy_score(true, predit) 
                 #校验集
                 dev_acc, dev_loss = evaluate(config, model, dev_iter)
                 if dev_loss < dev_best_loss:
                     dev_best_loss = dev_loss
                     torch.save(model.state_dict(), config.save_path)
-                    improve = '*'  #loss在下降每步输出就打星
-                    last_improve = total_batch
+                    imporve = '*'
+                    last_imporve = total_batch
                 else:
-                    improve = ''
+                    imporve = ''
                 time_dif = utils.get_time_dif(start_time)
                 msg = 'Iter:{0:>6}, Train Loss:{1:>5.2}, Train Acc:{2:>6.2}, Val Loss:{3:>5.2}, Val Acc:{4:>6.2%}, Time:{5} {6}'
-                print(msg.format(total_batch, loss.item(), train_acc, dev_loss, dev_acc, time_dif, improve))
+                print(msg.format(total_batch, loss.item(), train_acc, dev_loss, dev_acc, time_dif, imporve))
                 model.train()
-            total_batch += 1     
-            if total_batch - last_improve > config.require_improvement:
+            total_batch = total_batch + 1
+            if total_batch - last_imporve > config.require_improvement:
+                #在验证集合上loss超过1000batch没有下降，结束训练
                 print('在校验数据集合上已经很长时间没有提升了，模型自动停止训练')
                 flag = True
                 break
@@ -74,30 +84,45 @@ def train(config, model, train_iter, dev_iter, test_iter):
     test(config, model, test_iter)
 
 
-def evaluate(config, model, iters, test=False):
+def evaluate(config, model, dev_iter, test=False):
+    """
+    :param config:
+    :param model:
+    :param dev_iter:
+    :return:
+    """
+    #启用评估模式  
     model.eval()
     loss_total = 0
-    labels_all = np.array([], dtype = int)
-    predict_all = np.array([], dtype = int)
+    predict_all = np.array([], dtype=int)
+    labels_all = np.array([], dtype=int)
     with torch.no_grad():
-        for iter, labels in iters:
-            outputs = model(iter)
+        for texts, labels in dev_iter:
+            outputs = model(texts)
             loss = F.cross_entropy(outputs, labels)
-            loss_total += loss 
-            label = labels.data.cpu().numpy()
-            predict = torch.max(outputs, 1)[1].cpu().numpy()
-            labels_all = np.append(labels_all, label)
+            loss_total = loss_total + loss
+            labels = labels.data.cpu().numpy()
+            predict = torch.max(outputs.data,1)[1].cpu().numpy()
+            labels_all = np.append(labels_all, labels)
             predict_all = np.append(predict_all, predict)
+
     acc = metrics.accuracy_score(labels_all, predict_all)
     if test:
-        report = metrics.classification_report(labels_all, predict_all, target_names=config.class_list, digits = 4)
+        report = metrics.classification_report(labels_all, predict_all, target_names=config.class_list, digits=4)
         confusion = metrics.confusion_matrix(labels_all, predict_all)
-        return acc, loss_total/len(iters), report, confusion
+        return acc, loss_total / len(dev_iter), report, confusion
 
-    return acc, loss_total/len(iters) 
+    return acc, loss_total / len(dev_iter)
 
 
 def test(config, model, test_iter):
+    """
+    模型测试
+    :param config:
+    :param model:
+    :param test_iter:
+    :return:
+    """
     model.load_state_dict(torch.load(config.save_path))
     model.eval()
     start_time = time.time()
@@ -110,6 +135,5 @@ def test(config, model, test_iter):
     print(test_confusion)
     time_dif = utils.get_time_dif(start_time)
     print("使用时间：",time_dif)
-
 
 
--- project1 Bert-text-classification/utils.py
+++ project1 Bert-text-classification/utils.py
@@ -7,50 +7,59 @@ import os
 
 PAD, CLS = '[PAD]', '[CLS]'
 
-#处理train.txt, dev.txt, test.txt的原始文件为模型可读
+'''
+dataset.pkl的格式  [(32, 1 , 1, 32),(32, 1 , 1, 32)]
+[([1, 132, 664, 19, 18, 833, 26, 21, 401, 166, 556, 228, 2211, 1568, 1860, 
+2785, 171, 650, 1625, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 3, 19, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),...........]
+'''
+
+#处理成bert的认识的格式
 def load_dataset(file_path, config):
-    '''
-    return [ids, label, ids_len, mask]
-    '''
+    """
+    返回结果 4个list  [ids, lable, ids_len, mask]
+    :param file_path:
+    :param seq_len:
+    :return:
+    """
     contents = []
-    with open(file_path, 'r', encoding = 'UTF-8') as f:
+    with open(file_path, 'r', encoding='UTF-8') as f:
         for line in tqdm(f):
             line = line.strip()
             if not line:
                 continue
-            content, label = line.split('\t')
+            content, lable = line.split('\t')
             token = config.tokenizer.tokenize(content)
             token = [CLS] + token
-            token_ids = config.tokenizer.convert_tokens_to_ids(token)
             seq_len = len(token)
             mask = []
-            
+            token_ids = config.tokenizer.convert_tokens_to_ids(token)
+
             pad_size = config.pad_size
+
             if pad_size:
-                if seq_len < pad_size:
-                    mask = [1] * seq_len + [0] * (pad_size - seq_len)
-                    token_ids = token_ids + [0] * (pad_size - seq_len)
+                if len(token) < pad_size:
+                    mask = [1] * len(token_ids) + [0] * (pad_size - len(token))
+                    token_ids = token_ids + ([0] * (pad_size - len(token)))
                 else:
                     mask = [1] * pad_size
                     token_ids = token_ids[:pad_size]
-                    seq_len = pad_size 
-            content = contents.append((token_ids, label, seq_len, mask))
-    return contents       
+                    seq_len = pad_size
+            contents.append((token_ids, int(lable), seq_len, mask))
+    return contents
 
 
 
-#处理dataset.pkl为模型可读
-def build_dataset(config):
-    '''
-    return train_data, dev_data, test_data
-    '''
-    #如果datasetpkl存在则直接取出返回train, dev, test
+def bulid_dataset(config):
+    """
+    返回值 train, dev ,test
+    :param config:
+    :return:
+    """
     if os.path.exists(config.datasetpkl):
         dataset = pkl.load(open(config.datasetpkl, 'rb'))
-        train = dataset['train']
+        train = dataset['train']  
         dev = dataset['dev']
         test = dataset['test']
-    #如果datasetpkl不存在则利用load_dataset处理train.txt, dev.txt, test.txt
     else:
         train = load_dataset(config.train_path, config)
         dev = load_dataset(config.dev_path, config)
@@ -59,63 +68,67 @@ def build_dataset(config):
         dataset['train'] = train
         dataset['dev'] = dev
         dataset['test'] = test
-        pkl.dump(dataset, open(config.datasetpkl, 'wb'))  #写入datasetpkl文件
+        pkl.dump(dataset, open(config.datasetpkl, 'wb'))
+    return train, dev, test
 
-    return train, dev, test 
 
 class DatasetIterator(object):
     def __init__(self, dataset, batch_size, device):
-        self.batch_size = batch_size 
+        self.batch_size = batch_size
         self.dataset = dataset
-        self.device = device
-        self.num_batches = len(self.dataset) // self.batch_size  # dataset有几个batch_size
-        self.residue = False    #记录batch是否有残余
-        if len(self.dataset) % self.num_batches != 0:
+        self.n_batches = len(dataset) // batch_size     # dataset有几个batch_size
+        self.residue = False      #记录batch数量是否为整数
+        if len(dataset) % self.n_batches != 0:
             self.residue = True
-        self.index = 0
-    
-    def _to_tensor(self, batch):
-        x = torch.LongTensor([item[0] for item in batch]).to(self.device)
-        y = torch.LongTensor([item[1] for item in batch]).to(self.device)
-        seq_len = torch.LongTensor([item[2] for item in batch]).to(self.device)
-        mask = torch.LongTensor([item[3] for item in batch]).to(self.device)
+        self.index = 0  #标记位 
+        self.device = device
+
+    def _to_tensor(self, datas):
+        x = torch.LongTensor([item[0] for item in datas]).to(self.device) #样本数据ids 
+        y = torch.LongTensor([item[1] for item in datas]).to(self.device) #标签数据label
+        seq_len = torch.LongTensor([item[2] for item in datas]).to(self.device) #每一个序列的真实长度
+        mask = torch.LongTensor([item[3] for item in datas]).to(self.device)
+
         return (x, seq_len, mask), y
-    
+
     def __next__(self):
-        if self.residue and self.index == self.num_batches:
-            batches = self.dataset[self.index * self.batch_size:]
+        if self.residue and self.index == self.n_batches:   # index为最后一个batch
+            batches = self.dataset[self.index * self.batch_size : len(self.dataset)]
             self.index += 1
             batches = self._to_tensor(batches)
             return batches
-        elif self.index > self.num_batches:
+
+        elif self.index > self.n_batches:   # 无数据可返
             self.index = 0
             raise StopIteration
-        else:
+
+        else:   # 开始返回每个batches
             batches = self.dataset[self.index * self.batch_size : (self.index + 1) * self.batch_size]
-            self.index += 1 
+            self.index += 1
             batches = self._to_tensor(batches)
-            return batches 
-    
+            return batches
+
     def __iter__(self):
         return self
-    
+
     def __len__(self):
         if self.residue:
-            return self.num_batches + 1
+            return self.n_batches + 1
         else:
-            return self.num_batches
+            return self.n_batches
 
 
-#处理数据为dataloader, 返回数据形式为 (x, seq_len, mask), y
-def build_iterator(dataset, config):
+def bulid_iterator(dataset, config):
     iter = DatasetIterator(dataset, config.batch_size, config.device)
-    return iter 
+    return iter
 
 
 def get_time_dif(start_time):
-    '''
-    获取使用时间
-    '''
+    """
+    获取已经使用的时间
+    :param start_time:
+    :return:
+    """
     end_time = time.time()
     time_dif = end_time - start_time
-    return timedelta(seconds = int(round(time_dif)))
+    return timedelta(seconds=int(round(time_dif)))
